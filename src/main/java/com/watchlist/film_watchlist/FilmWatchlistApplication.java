ae59c4de3ca3be1e32a5fc83e8ad7b29b7e53ebd
Velja15 <dimitrijevicveljko04@gmail.com>
2026-04-24 09:26:50 +0200

    Initial commit spring boot projekat

---
+++ src/main/java/com/watchlist/film_watchlist/FilmWatchlistApplication.java
@@ -0,0 +1,13 @@
+package com.watchlist.film_watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmWatchlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watchlist/film_watchlist/FilmWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchlist.film_watchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
